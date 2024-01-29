e18e98bbeba513d5862aa9c1f63f81a8d4c0a19e
Cristhian <cristhian.panora@gmail.com>
2024-01-29 16:44:10 -0500

    Configuraciones

---
+++ src/components/Formulario.jsx
@@ -0,0 +1,161 @@
+import { useState } from "react"
+import Mensajes from "./Mensajes"
+import { v4 as uuidv4 } from 'uuid';
+
+export const Formulario = ({setEstado}) => {
+
+    const [error, setError] = useState(false)
+    const [mensaje, setMensaje] = useState(false)
+    const [form, setform] = useState({
+        nombre:"",
+        sector:"",
+        salida:"",
+        llegada:"",
+        maquinista:"",
+        detalles:""
+    })
+
+    const handleChange = (e) => { 
+        setform({
+            ...form, // copiar las propiedades del objeto inicial
+            [e.target.name]: e.target.value.trim() //actualicar las propiedades, con trim elimina los espacios y eso puede dar errores si se intenta agregar un espacio en el formulario
+        })
+     }
+
+     const handleSubmit = async(e)=>{
+        e.preventDefault()
+        // validacion
+        if (Object.values(form).includes("") || Object.entries(form).length === 0)
+        {
+            setError(true) //cambiar el valor a la variable error
+            setTimeout(() => {
+                setError(false) //cambiar el valor a la variable error despues de 1 segundo
+            }, 1000);
+            return
+        }
+        try {
+            const url ="https://65b819fb46324d531d55f23f.mockapi.io/api/metro/" // api
+			form.id = uuidv4() // agregar una propiedad de id
+            await fetch(url,{
+                method:'POST', // especificar el tipo de accion 
+                body:JSON.stringify(form), // convertir la variable form a json
+                headers:{'Content-Type':'application/json'} // especificar el tipo de contenido
+            })
+            setMensaje(true)
+			setEstado(true)
+            setTimeout(() => {
+                setMensaje(false)
+                setEstado(false)
+                setform({})
+            }, 1000);
+        } catch (error) {
+            console.log(error);
+        }
+
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+
+            {error && <Mensajes tipo="bg-red-900">"Existen campos vacíos"</Mensajes>}
+	        {mensaje && <Mensajes tipo="bg-green-900">"Registro exitoso"</Mensajes>}
+
+            {/* <Mensajes tipo={"bg-red-900"}>validar campos</Mensajes> */}
+            <div>
+                <label
+                    htmlFor='nombre'
+                    className='text-gray-700 uppercase font-bold text-sm'>Nombre: </label>
+                <input
+                    id='nombre'
+                    type="text"
+                    className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md mb-5'
+                    placeholder='nombre de la ruta'
+                    name='nombre'
+                    value={form.nombre || ""} 
+                    onChange={handleChange}
+                />
+            </div>
+
+            <div>
+                <label
+                    htmlFor='sector'
+                    className='text-gray-700 uppercase font-bold text-sm'>Sector: </label>
+                <input
+                    id='sector'
+                    type="text"
+                    className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md mb-5'
+                    placeholder='sector de la ruta'
+                    name='sector'
+                    value={form.sector || ""} 
+                    onChange={handleChange}
+                />
+            </div>
+
+            <div>
+                <label
+                    htmlFor='salida'
+                    className='text-gray-700 uppercase font-bold text-sm'>Punto de salida: </label>
+                <input
+                    id='salida'
+                    type="text"
+                    className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md mb-5'
+                    placeholder='punto de salida'
+                    name='salida'
+                    value={form.salida || ""} 
+                    onChange={handleChange}
+                />
+            </div>
+
+            <div>
+                <label
+                    htmlFor='llegada'
+                    className='text-gray-700 uppercase font-bold text-sm'>Punto de llegada: </label>
+                <input
+                    id='llegada'
+                    type="text"
+                    className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md mb-5'
+                    placeholder='punto de llegada'
+                    name='llegada'
+                    value={form.llegada || ""} 
+                    onChange={handleChange}
+                />
+            </div>
+
+            <div>
+                <label
+                    htmlFor='maquinista'
+                    className='text-gray-700 uppercase font-bold text-sm'>Nombre del maquinista: </label>
+                <input
+                    id='maquinista'
+                    type="text"
+                    className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md mb-5'
+                    placeholder='nombre del maquinista'
+                    name='maquinista'
+                    value={form.maquinista || ""} 
+                    onChange={handleChange}
+                />
+            </div>
+            <div>
+                <label
+                    htmlFor='detalles'
+                    className='text-gray-700 uppercase font-bold text-sm'>Detalles: </label>
+                <textarea
+                    id='detalles'
+                    type="text"
+                    className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md mb-5'
+                    name='detalles'
+                    value={form.detalles || ""} 
+                    onChange={handleChange}
+                />
+            </div>
+
+            <input
+                type="submit"
+                className='bg-sky-900 w-full p-3 
+        text-white uppercase font-bold rounded-lg 
+        hover:bg-red-900 cursor-pointer transition-all'
+                value='Registrar ruta' />
+
+        </form>
+    )
+}
---
+++ src/components/Listar.jsx
@@ -0,0 +1,64 @@
+import { useState, useEffect } from "react"
+import logoMetro from '../assets/tren.webp'
+import Mensajes from './Mensajes'
+
+const Listar = ({estado}) => {
+
+    const [rutas, setRutas] = useState([])
+
+    useEffect(() => {
+
+        // veridicar si la variable estado se ha cambiado
+        // verificar el tamaño del arreglo rutas
+        if(estado || rutas.length>=0)
+        {
+            (async function () {
+                try {
+                    // fetch a la api y convertir en json la respuesta
+                    const respuesta = await (await fetch("https://65b819fb46324d531d55f23f.mockapi.io/api/metro/")).json()
+                    // asignar el resultado a la variable setRutas
+                    setRutas(respuesta)
+                    console.log("petición");
+                }
+                catch (error) {
+                    console.log(error);
+                }
+            })()
+        }
+}, [estado])
+
+    return (
+        <>
+            {
+          rutas.length === 0
+              ?
+              <Mensajes tipo={"bg-green-900"}>"No existe rutas creadas"</Mensajes>
+              :
+              rutas.map((ruta) => (
+                  <div key={ruta.id} className="p-2 rounded-xl sm:flex gap-12 bg-gray-200 shadow-xl mb-5">
+
+                      <img src={logoMetro} alt="art cover" className="sm:w-3/12 object-cover rounded-lg" />
+
+                      <div className="h-auto p-3 w-full">
+                          <h4 className="text-2xl font-semibold text-cyan-900">{ruta.nombre}</h4>
+                          <hr className="w-full border border-gray-300 my-2" />
+                          <p className="text-gray-500">Sector: {ruta.sector}</p>
+                          <p className="text-gray-500">Punto de salida: {ruta.salida}</p>
+                          <p className="text-gray-500">Punto de llegada: {ruta.llegada}</p>
+                          <p className="text-gray-500">Maquinista: {ruta.maquinista}</p>
+                          <p className="text-gray-500">Detalles: {ruta.detalles}</p>
+                          <div className='flex justify-between mt-3 lg:justify-end md:justify-end gap-3'>
+                              <button className='bg-sky-900 text-white px-6 py-1 rounded-full'>Actualizar</button>
+                              <button className='bg-red-900 text-white px-6 py-1 rounded-full'>Eliminar</button>
+                          </div>
+                      </div>
+                  </div>
+              ))
+      }
+
+        </>
+
+    )
+}
+
+export default Listar
